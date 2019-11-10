ceff54520d8c2c7a4f69c86d1b62183cf31bee6f
Michael Le <michael2017le@gmail.com>
2019-11-10 12:00:00 -0600

    ;a'

--- filterNews.py
+++ filterNews.py
@@ -4,23 +4,26 @@ from newspaper import Article
 d = unsortedMap.makeDictionary()
 
 def filter(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    # print(article.title)
-    avg = 0
-    count = 0
-    # print(article.text)
+    try:
+      article = Article(url)
+      article.download()
+      article.parse()
+      article.nlp()
+      # print(article.title)
+      avg = 0
+      count = 0
+      # print(article.text)
 
-    for word in article.text:
-        if word in d:
-            avg += d[word]
-        count += 1
-    
-    # print("Score: " + str(avg / count))
+      for word in article.text:
+          if word in d:
+              avg += d[word]
+          count += 1
+      
+      # print("Score: " + str(avg / count))
+      return (article.title, avg/count, url)
+    except:
+      return None
 
-    return (article.title, avg/count, url)
 
 if __name__ == '__main__':
     filter('https://cnn.com/2019/11/09/politics/trump-balloon-alabama-lsu-football-game/index.html')
