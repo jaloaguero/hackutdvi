78dca644310fa608d5a3777ac116e0a97804794c
Michael Le <michael2017le@gmail.com>
2019-11-10 07:00:59 -0600

    integrated frontend and backend and database

--- main.py
+++ main.py
@@ -11,18 +11,26 @@ app = Flask(__name__)
 
 client = pymongo.MongoClient(os.getenv('MONGO_SRV'), connect=False, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 articles = client.data.articles
+known_news = [news['name'] for news in client.data.news.find()]
+
+print(known_news)
 
 @app.route('/')
 def home():
+  return render_template('homepage.html')
+
+@app.route('/sources')
+def sources():
   return render_template('index.html')
 
-@app.route('/cnn')
-def cnn():
-  result = []
-  for article in articles.find({'source': 'CNN'}):
-    article['_id'] = str(article['_id'])
-    result.append(article)
-  return json.dumps(result)
+@app.route('/<news>')
+def article_list(news):
+  news = news.upper()
+  if news in known_news:
+    news_articles = [article for article in articles.find({'source': news})]
+    return render_template('article_page.html', news=news, news_articles=news_articles)
+  else:
+    return '404'
 
 if __name__ == '__main__':
   app.run(debug=True)
