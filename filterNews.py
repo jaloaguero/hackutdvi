18e3980a63896a884f74e498bade604709ddf830
enegronjr <eddienegronjr@gmail.com>
2019-11-10 08:03:20 -0600

    first push

--- filterNews.py
+++ filterNews.py
@@ -1,13 +1,26 @@
 import unsortedMap
+from newspaper import Article
 
 d = unsortedMap.makeDictionary()
-print(d)
 
-while True:
-	msg = input("Enter a word to search\n")
-	if msg not in d:
-		print('0.0')
-	else:
-		print(d[msg])
+def filter(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    article.nlp()
+    print(article.title)
+    avg = 0
+    count = 0
+    # print(article.text)
 
-	print("\nNEXT")
+    for word in article.text:
+        if word in d:
+            avg += d[word]
+        count += 1
+    
+    print("Score: " + str(avg / count))
+
+    return (article.title, avg/count, url)
+
+if __name__ == '__main__':
+    filter('https://cnn.com/2019/11/09/politics/trump-balloon-alabama-lsu-football-game/index.html')
--- unsortedMap.py
+++ unsortedMap.py
@@ -6,11 +6,12 @@ wb = xlrd.open_workbook('wods.xlsx')
 sh = wb.sheet_by_index(0)
 
 def makeDictionary():
-	for i in range(1, 117660):
-		pos_value = float(sh.cell(i,0).value)
-		neg_value = float(sh.cell(i,1).value)
-		words = sh.cell(i,2).value
-		for word in words.split():
-			d[word[:-2].replace('_', ' ')] = 1 - (pos_value + neg_value)
-	return d
+    for i in range(1,117660):
+        pos_value = float(sh.cell(i,0).value)
+        neg_value = float(sh.cell(i,1).value)
+        words = sh.cell(i,2).value
+        for word in words.split():
+            d[word[:-2].replace('_',' ')] = 1 - (pos_value + neg_value)
+        
 
+    return d
