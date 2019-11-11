6173fec85bba6781c2806964cff5cf71316835c7
Michael Le <michael2017le@gmail.com>
2019-11-11 09:03:13 -0600

    last commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 import os
 import ssl
 from dotenv import load_dotenv
-from filterNews import filter
+from filterNews import filter, d
+from highlight import highlight
 
 load_dotenv()
 
@@ -25,7 +26,8 @@ def enter_url():
   if request.method == 'GET':
     return render_template('enter_url.html')
   else:
-    return str(filter(request.form['url']))
+    highlight(request.form['url'], d)
+    return render_template('highlighted_article.html')
 
 @app.route('/sources')
 def sources():
@@ -39,6 +41,18 @@ def article_list(news):
     return render_template('article_page.html',
                            news=news,
                            news_articles=news_articles)
+  else:
+    return '404'
+
+@app.route('/<news>/<article_url>')
+def show_highlight(news, article_url):
+  news = news.upper()
+  article_url = articles.find_one({'title': article_url})['url']
+  print(article_url)
+  if news in known_news:
+#     article_url = 'https://www.foxnews.com/us/man-arrested-for-stabbing-baby-trump-protest-balloon-at-alabama-lsu-game'
+    highlight(article_url, d)
+    return render_template('highlighted_article.html')
   else:
     return '404'
 
